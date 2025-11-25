2b6a94e1424300e0a205b5caede0ffab0dfa9a92
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-11-25 22:12:35 +0530

    write file

--- File_input_output/app.py
+++ File_input_output/app.py
@@ -1,11 +1,22 @@
-def file_name(fileName: str):
+def file_Read(fileName: str):
     try:
-        file = open(fileName, 'r')
-        content = file.read()
+        fileRead = open(fileName, 'r')
+        content = fileRead.read()
         print("File Content:")
         print(content)
+        fileRead.close()
     except FileNotFoundError:
         print(f"The file {fileName} does not exist.")
-
+        
+def file_Write(fileName: str, data: str = "Sample data to write to file."):
+    try:
+        fileWrite = open(fileName, 'w')
+        fileWrite.write(data)
+        fileWrite.close()
+    except Exception as e:
+        print(f"An error occurred while writing to the file: {e}")
+        
 fileName = 'data.csv'
-file_name(fileName)
+file_Read(fileName)
+file_Write(fileName,'this is empty file now')
+file_Read(fileName)
