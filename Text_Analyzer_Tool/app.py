fa90d85d2534be4afcb564dd3d6d6b2e03bec581
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-11-27 19:43:55 +0530

    word counting done

--- Text_Analyzer_Tool/app.py
+++ Text_Analyzer_Tool/app.py
@@ -1,6 +1,17 @@
+from read_file import read
 
 def main():
     print("Welcome to the Text Analyzer Tool!")
+    try:
+        file_path = 'content_file.txt'
+        content = read(file_path)
+        if content:
+            pass
+        else:
+            print("No content to analyze.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
     
 
 if __name__ == "__main__":
---
+++ Text_Analyzer_Tool/count_words.py
@@ -0,0 +1,32 @@
+
+def get_word(text: str)-> int:
+    if(len(text) == 0 or not isinstance(text, str)):
+        return 0
+    trimed_text = text.strip()
+    words = {
+        'word_count': 0,
+        'words': [],
+        'unique_words': set(),
+        'char_count_with_space': 0,
+        'char_count_without_space': 0
+    }
+    
+    text = trimed_text.lower()
+    word = ''
+    skip_char = ['.',',','!','?',';',':']
+    for char in text:
+        words['char_count_with_space'] += 1
+        if char != ' ' and char != '\n' and char != '\t' and char != '\r':
+            words['char_count_without_space'] += 1
+        if (char == ' ' or char == '\n' or char == '\t' or char == '\r') and word != '':
+            words['words'].append(word)
+            words['unique_words'].add(word)
+            words['word_count'] += 1
+            word = ''
+        elif char != ' ' and char != '\n' and char != '\t' and char != '\r' and char not in skip_char:
+            word += char
+    if word != '':
+        words['words'].append(word)
+        words['unique_words'].add(word)
+        words['word_count'] += 1
+    return words
