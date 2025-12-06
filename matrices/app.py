bc8700e91f35dc37ac02f620128e48bfddcbb604
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-12-07 00:13:38 +0530

    matix broadcasting

--- matrices/app.py
+++ matrices/app.py
@@ -63,4 +63,38 @@ b = np.array([
 # matrices_addtions(a,b)
 # matrices_subtractions(a,b)
 
-print(matrices_multiplication(a,b))
+# print(matrices_multiplication(a,b))
+
+def test():
+    # arr = np.array([
+    #     [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10],
+    #     [11,12,13,14,15,16,17,18,19,20],
+    #     [21,22,23,24,25,26,27,28,29,30],
+    #     [31,32,33,34,35,36,37,38,39,40],
+    #     [41,42,43,44,45,46,47,48,49,50],
+    #     [51,52,53,54,55,56,57,58,59,60],
+    #     [61,62,63,64,65,66,67,68,69,70],
+    #     [71,72,73,74,75,76,77,78,79,80],
+    #     [81,82,83,84,85,86,87,88,89,90],
+    #     [91,92,93,94,95,96,97,98,99,100]
+    # ])
+    
+    # print(arr)
+    # print("Print All Elements of first Row")
+    # print(arr[0:1,:])
+    # print("\n Print All Elements of first column")
+    # print(arr[:,0:1])
+    # print("\n Print middle element")
+    # print(arr[2:,1:2])
+    # print("\n check minus")
+    # print(arr[-1:,-1:])
+    # print("\nEvery other element in last row:", arr[0, 2::2])
+    # print(arr++1)
+    arr1 = np.array([
+            [1,2,3],
+            [4,5,6]
+        ])
+    arr2 = np.array([7,9])
+    
+    print(arr1 + arr2)  # Broadcasting in action
+test()
